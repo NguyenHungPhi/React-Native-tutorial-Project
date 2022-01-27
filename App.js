284f4d409278c49e0b5ac73a2e17f12c9a7a4479
phi <nguyen@gmail.com>
2022-01-27 23:20:43 +0700

    Add navigation

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import * as React from 'react';
+import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {Text} from 'react-native';
-const App = () => {
+import {createStackNavigator} from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <Text>One step</Text>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default App;
